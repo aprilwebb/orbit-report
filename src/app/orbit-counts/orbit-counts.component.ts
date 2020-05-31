a98763b3f33047dea3f641ac8fcd55a40ca3424d
April Webb <aprilewebb@gmail.com>
2020-05-31 15:56:12 -0400

    Added zebra stripes and modified search feature

---
+++ src/app/orbit-counts/orbit-counts.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { Satellite } from '../satellite';
+
+@Component({
+  selector: 'app-orbit-counts',
+  templateUrl: './orbit-counts.component.html',
+  styleUrls: ['./orbit-counts.component.css']
+})
+export class OrbitCountsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  count() {
+
+  }
+}
