0e0742cde98084e71b6e617ab02c98371aadafa2
April Webb <aprilewebb@gmail.com>
2020-06-02 21:04:54 -0400

    Modified search function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -36,7 +36,8 @@ export class AppComponent {
     for (let i = 0; i < this.sourceList.length; i++) {
       let name = this.sourceList[i].name.toLowerCase();
       let type = this.sourceList[i].type.toLowerCase();
-      if (name.indexOf(searchTerm) >= 0 || type.indexOf(searchTerm) >= 0) {
+      let orbitType = this.sourceList[i].orbitType.toLowerCase();
+      if (name.indexOf(searchTerm) >= 0 || type.indexOf(searchTerm) >= 0 || orbitType.indexOf(searchTerm) >= 0) {
         matchingSatellites.push(this.sourceList[i]);
       }
     }
